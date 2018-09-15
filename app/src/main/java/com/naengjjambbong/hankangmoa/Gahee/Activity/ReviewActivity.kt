589b96dfda60d762fa300efe58c78733e4240e75
yougahee <dbrkgml97@nate.com>
2018-09-15 23:03:40 +0900

    Merge branch 'master' of https://github.com/project-hankangmoa/project-hankangmoa-Android
    # Conflicts:
    #	app/src/main/res/drawable-hdpi/btn_image_edit.png
    #	app/src/main/res/drawable-xhdpi/btn_image_edit.png
    #	app/src/main/res/drawable-xxhdpi/btn_image_edit.png
    #	app/src/main/res/drawable-xxxhdpi/btn_image_edit.png

--- app/src/main/java/com/naengjjambbong/hankangmoa/Gahee/Activity/ReviewActivity.kt
+++ app/src/main/java/com/naengjjambbong/hankangmoa/Gahee/Activity/ReviewActivity.kt
@@ -1,6 +1,7 @@
 package com.naengjjambbong.hankangmoa.Gahee.Activity
 
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.naengjjambbong.hankangmoa.R
 
 class ReviewActivity : AppCompatActivity() {
